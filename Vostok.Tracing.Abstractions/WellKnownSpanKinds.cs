0c764840d53cf67febd006a3dc8d6715aaab80ab
iloktionov <iloktionov@skbkontur.ru>
2018-08-30 17:50:27 +0500

    Minor refactoring and renames in WellKnownSpanKinds.

--- Vostok.Tracing.Abstractions/WellKnownSpanKinds.cs
+++ Vostok.Tracing.Abstractions/WellKnownSpanKinds.cs
@@ -5,26 +5,32 @@ namespace Vostok.Tracing.Abstractions
     [PublicAPI]
     public static class WellKnownSpanKinds
     {
-        public const string Database = "db-request";
-
         public static class HttpRequest
         {
             public const string Client = HttpRequestPrefix + "client";
             public const string Server = HttpRequestPrefix + "server";
             public const string Cluster = HttpRequestPrefix + "cluster";
+
             private const string HttpRequestPrefix = "http-request-";
         }
 
         public static class Queue
         {
-            private const string QueuePrefix = "queue-";
-            private const string TaskPrefix = "task-";
-
             public const string Producer = QueuePrefix + "producer";
             public const string Manager = QueuePrefix + "manager";
             public const string Consumer = QueuePrefix + "consumer";
             public const string TaskLifecycle = QueuePrefix + TaskPrefix + "lifecycle";
             public const string TaskLifecycleEvent = QueuePrefix + TaskPrefix + "lifecycle-event";
+
+            private const string QueuePrefix = "queue-";
+            private const string TaskPrefix = "task-";
+        }
+
+        public static class Database
+        {
+            public const string Request = "db-request";
+
+            private const string DatabasePrefix = "db-";
         }
     }
 }
