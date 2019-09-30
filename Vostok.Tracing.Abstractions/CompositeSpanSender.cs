dbacaa3edd3f59c013b093673ecf8cd1456e3cc3
kungurtsev <kungurtsev@skbkontur.ru>
2019-09-30 15:19:10 +0500

    added CompositeSpanSender

---
+++ Vostok.Tracing.Abstractions/CompositeSpanSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Tracing.Abstractions
+{
+    /// <summary>
+    /// A span sender that passes spans on to all of the base logs.
+    /// </summary>
+    [PublicAPI]
+    public class CompositeSpanSender : ISpanSender
+    {
+        private readonly ISpanSender[] baseSpanSenders;
+
+        public CompositeSpanSender(params ISpanSender[] baseSpanSenders)
+        {
+            this.baseSpanSenders = baseSpanSenders ?? throw new ArgumentNullException(nameof(baseSpanSenders));
+        }
+
+        public void Send(ISpan span)
+        {
+            foreach (var baseSpanSender in baseSpanSenders)
+                baseSpanSender.Send(span);
+        }
+    }
+}
